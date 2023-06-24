28ec44bd07037c2c3ccc4f3738e113f65fb888a6
Vigneshwaran T <tvignesh835@gmail.com>
2023-06-25 00:19:52 +0530

    changes 2

---
+++ slice_splice.js
@@ -0,0 +1,25 @@
+// splice()
+// array.splice(start, deleteCount, item1, item2);
+// splice() examples
+// const animals = ['Lion', 'Tiger', 'Black Panther'];
+// const modAnimals = animals.splice(1,1); // Delete
+// animals.splice(4,0,'Leapord'); // Insert Elements
+// animals.splice(1,1,'Cat'); // Replace Elements
+// console.log(animals); // Output: [ 'Lion', 'Black Panther' ]
+// console.log(modAnimals); // Output: [ 'Tiger' ]
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// slice()
+// array.slice(start, end)
+// slice() Examples
+const fruits = ['Apple', 'Orange', 'Mango', 'Banana'];
+console.log(fruits); // Output: [ 'Apple', 'Orange', 'Mango', 'Banana' ]
+
+const fruitsCopy = fruits.slice();
+console.log(fruitsCopy); // Output: [ 'Apple', 'Orange', 'Mango', 'Banana' ]
+
+const fruitsSlice = fruits.slice(1,3);
+console.log(fruitsSlice); // Output: [ 'Orange', 'Mango' ]
+
+
--- spread_rest.js
+++ spread_rest.js
@@ -1,9 +1,48 @@
-// splice()
-// array.splice(start, deleteCount, item1, item2);
-// splice() examples
-const animals = ['Lion', 'Tiger', 'Black Panther'];
-// const modAnimals = animals.splice(1,1); // Delete
-animals.splice(4,0,'Leapord'); // Insert Elements
-animals.splice(1,1,'Cat'); // Replace Elements
-console.log(animals); // Output: [ 'Lion', 'Black Panther' ]
-// console.log(modAnimals); // Output: [ 'Tiger' ]
+/////////////////////////////////////// Spread operator //////////////////////////////////////////
+//// Copy Array
+// const originalArray = [1, 2, 3];
+// const copiedArray = [...originalArray];
+// copiedArray.push(4);
+// console.log(originalArray); // [1, 2, 3]
+// console.log(copiedArray); // [1, 2, 3]
+
+//// Concatenating arrays
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+
+// const concatArray = [...array1, ...array2];
+
+// console.log(array1); // [1, 2, 3]
+// console.log(array2); // [4, 5, 6]
+// console.log(concatArray); // [ 1, 2, 3, 4, 5, 6 ]
+
+//// Spreading elements in function arguments
+// function sum(a, b, c) {
+//     return a + b + c;
+// }
+
+// const array = [1, 2, 3];
+// const arraySum = sum(...array);
+
+// console.log(arraySum); //6
+
+//// Creating a shallow copy of an object
+// const originalObject = { name: 'John', age: 30 };
+// const copiedObject = { ...originalObject };
+// console.log(copiedObject); // { name: 'John', age: 30 }
+
+//// Mergin Objects
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { c: 3, d: 4 };
+// const mergedObject = { ...obj1, ...obj2 };
+// console.log(mergedObject); // { a: 1, b: 2, c: 3, d: 4 }
+
+
+
+/////////////////////////////////////// Rest operator ////////////////////////////////////////////
+
+const arrayEle = (...args) => {
+    return args;
+}
+
+console.log(arrayEle(1, 'One', 2, 'Two'));
