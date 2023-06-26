ee2f6b198cbf7c9073d655ce75bda0d18ed0ffc1
Vigneshwaran T <tvignesh835@gmail.com>
2023-06-27 00:24:07 +0530

    Changes 5

--- JavaScript Refresher/slice_splice.js
+++ JavaScript Refresher/slice_splice.js
@@ -1,25 +1,25 @@
-// splice()
-// array.splice(start, deleteCount, item1, item2);
-// splice() examples
+// // splice()
+// // array.splice(start, deleteCount, item1, item2);
+// // splice() examples
 // const animals = ['Lion', 'Tiger', 'Black Panther'];
 // const modAnimals = animals.splice(1,1); // Delete
 // animals.splice(4,0,'Leapord'); // Insert Elements
 // animals.splice(1,1,'Cat'); // Replace Elements
-// console.log(animals); // Output: [ 'Lion', 'Black Panther' ]
+// console.log(animals); // Output: [ 'Lion', 'Cat', 'Leapord' ]
 // console.log(modAnimals); // Output: [ 'Tiger' ]
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-// slice()
-// array.slice(start, end)
-// slice() Examples
-const fruits = ['Apple', 'Orange', 'Mango', 'Banana'];
-console.log(fruits); // Output: [ 'Apple', 'Orange', 'Mango', 'Banana' ]
+// // slice()
+// // array.slice(start, end)
+// // slice() Examples
+// const fruits = ['Apple', 'Orange', 'Mango', 'Banana'];
+// console.log(fruits); // Output: [ 'Apple', 'Orange', 'Mango', 'Banana' ]
 
-const fruitsCopy = fruits.slice();
-console.log(fruitsCopy); // Output: [ 'Apple', 'Orange', 'Mango', 'Banana' ]
+// const fruitsCopy = fruits.slice();
+// console.log(fruitsCopy); // Output: [ 'Apple', 'Orange', 'Mango', 'Banana' ]
 
-const fruitsSlice = fruits.slice(1,3);
-console.log(fruitsSlice); // Output: [ 'Orange', 'Mango' ]
+// const fruitsSlice = fruits.slice(1,3);
+// console.log(fruitsSlice); // Output: [ 'Orange', 'Mango' ]
 
 
